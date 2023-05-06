ffeec6a8ac550a737166ccd346cf3f2f5e2e3e13
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-05-06 15:07:15 -0300

    service modelo pronta (bug no cadastrar se ja tem dados no banco)

--- src/main/java/br/com/uniamerica/estacionamento/controller/ModeloController.java
+++ src/main/java/br/com/uniamerica/estacionamento/controller/ModeloController.java
@@ -16,13 +16,8 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/api/modelo")
 public class ModeloController {
-
-    @Autowired
-    private ModeloRepository modeloRepository;
     @Autowired
     private ModeloService modeloService;
-    @Autowired
-    private VeiculoRepository veiculoRepository;
 
     /* Sem usar Autowired
     public ModeloController(ModeloRepository modeloRepository){
@@ -43,21 +38,18 @@ public class ModeloController {
     @GetMapping
     public ResponseEntity<?> findByIdRequest(@RequestParam("id") final Long id){
         final Modelo modelo = this.modeloService.findById(id);
-
         return ResponseEntity.ok(modelo);
     }
 
     @GetMapping("/lista")
     public ResponseEntity<?> findAll(){
         final List<Modelo> modelos = this.modeloService.findAll();
-
         return ResponseEntity.ok(modelos);
     }
 
     @GetMapping("/ativos")
     public ResponseEntity<?> findByAtivo(){
         final List<Modelo> modelos = this.modeloService.findByAtivo();
-
         return ResponseEntity.ok(modelos);
     }
 
@@ -69,53 +61,12 @@ public class ModeloController {
 
     @PutMapping
     public ResponseEntity<?> editar(@RequestParam("id") final Long id, @RequestBody final Modelo modelo){
-        try{
-            final Modelo modeloBanco = this.modeloRepository.findById(id).orElse(null);
-
-            if(modeloBanco == null || !modeloBanco.getId().equals(modelo.getId()))
-            {
-                throw new RuntimeException("Não foi possível identificar o registro informado");
-            }
-
-            this.modeloRepository.save(modelo);
-            return ResponseEntity.ok("Registro editado com sucesso");
-        }
-        catch (DataIntegrityViolationException e){
-            return ResponseEntity.internalServerError().body("Error " + e.getCause().getCause().getMessage());
-        }
-        catch (RuntimeException e){
-            return ResponseEntity.internalServerError().body("Error " + e.getMessage());
-        }
+        this.modeloService.editar(id, modelo);
+        return ResponseEntity.ok("Registro editado com sucesso");
     }
 
     @DeleteMapping
     public ResponseEntity<?> excluir(@RequestParam("id") final Long id){
-        try {
-            final Modelo modelo = this.modeloRepository.findById(id).orElse(null);
-            if(modelo == null){
-                throw new Exception("Registro inexistente");
-            }
-
-            final List<Veiculo> veiculos = this.veiculoRepository.findAll();
-
-            for(Veiculo veiculo : veiculos){
-                if(modelo.equals(veiculo.getModelo())){
-                    modelo.setAtivo(false);
-                    this.modeloRepository.save(modelo);
-                    return ResponseEntity.ok("Registro não está mais ativo");
-                }
-            }
-
-            if(modelo.isAtivo()){
-                this.modeloRepository.delete(modelo);
-                return ResponseEntity.ok("Registro deletado com sucesso");
-            }
-            else{
-                throw new Exception("Não foi possível excluir o registro");
-            }
-        }
-        catch (Exception e){
-            return ResponseEntity.internalServerError().body("Error" + e.getMessage());
-        }
+        return ResponseEntity.ok(this.modeloService.excluir(id));
     }
 }
--- src/main/java/br/com/uniamerica/estacionamento/service/ModeloService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/ModeloService.java
@@ -2,9 +2,12 @@ package br.com.uniamerica.estacionamento.service;
 
 import br.com.uniamerica.estacionamento.entity.Marca;
 import br.com.uniamerica.estacionamento.entity.Modelo;
+import br.com.uniamerica.estacionamento.entity.Veiculo;
 import br.com.uniamerica.estacionamento.repository.MarcaRepository;
 import br.com.uniamerica.estacionamento.repository.ModeloRepository;
+import br.com.uniamerica.estacionamento.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -13,6 +16,7 @@ import javax.swing.text.html.Option;
 import java.beans.Transient;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 @Service
 public class ModeloService {
@@ -20,49 +24,68 @@ public class ModeloService {
     private ModeloRepository modeloRepository;
     @Autowired
     private MarcaRepository marcaRepository;
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+//    @Autowired
+//    private MarcaService marcaService;
+//    @Autowired
+//    private VeiculoService veiculoService;
 
     public Modelo findById(final Long id){
-
         return this.modeloRepository.findById(id).orElse(null);
     }
 
     public List<Modelo> findAll(){
-
         return this.modeloRepository.findAll();
     }
 
     public List<Modelo> findByAtivo(){
-
         return this.modeloRepository.findByAtivoIsTrue();
     }
 
     @Transactional(rollbackFor = Exception.class)
     public void cadastrar(final Modelo modelo){
-        Modelo modeloDatabase = this.modeloRepository.findByModelo(modelo.getModelo());
-        Assert.isNull(modeloDatabase, "Modelo já cadastrado!");
-
+        //Arrumar bug com put, ou colocar Setter no id e adicionar id pelo codigo
         Marca marca = this.marcaRepository.findById(modelo.getMarca().getId()).orElse(null);
         Assert.notNull(marca, "Marca não está cadastrada!");
         if(!marca.isAtivo()){
             marca.setAtivo(true);
-            this.marcaRepository.save(marca);
+            this.marcaRepository.save(marca);   //Trocar pelo marcaService depois
         }
+
+        Modelo modeloDatabase = this.modeloRepository.findByModelo(modelo.getModelo());
+        Assert.isNull(modeloDatabase, "Modelo já cadastrado!");
+        Assert.isTrue(!(modelo.getModelo().length() < 3), "Nome de modelo inválido!");
 
         this.modeloRepository.save(modelo);
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public void editar(final Modelo modelo){
-        Modelo modeloDatabase = this.modeloRepository.findByModelo(modelo.getModelo());
-        Assert.isNull(modeloDatabase, "Modelo já cadastrado!");
+    public void editar(final Long id, final Modelo modelo){
+        Modelo modeloDatabase = findById(id);
+        Assert.notNull(modeloDatabase, "Modelo não encontrado!");
+        Assert.isTrue(modeloDatabase.getId().equals(modelo.getId()), "Modelos não conferem!");
 
-        Marca marca = this.marcaRepository.findById(modelo.getMarca().getId()).orElse(null);
-        Assert.notNull(marca, "Marca não está cadastrada!");
-        if(!marca.isAtivo()){
-            marca.setAtivo(true);
-            this.marcaRepository.save(marca);
+        cadastrar(modelo);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public String excluir(final Long id){
+        Modelo modelo = findById(id);
+        Assert.notNull(modelo, "Modelo não encontrado!");
+        Assert.isTrue(modelo.getId().equals(modelo.getId()), "Modelos não conferem!");
+
+        final List<Veiculo> veiculos = this.veiculoRepository.findAll();    //Trocar pelo veiculoService depois
+        for(Veiculo veiculo : veiculos){
+            if(modelo.equals(veiculo.getModelo())){
+                modelo.setAtivo(false);
+                this.modeloRepository.save(modelo);
+                return "Modelo está inativo!";
+            }
         }
 
-        this.modeloRepository.save(modelo);
+        this.modeloRepository.delete(modelo);
+        return "Modelo excluido com sucesso!";
     }
 }
